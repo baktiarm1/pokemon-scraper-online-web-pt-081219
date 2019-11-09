eb324d98077d08043480cecca0bc66355fd628a0
Baktiar Miah <baktiar.m@gmail.com>
2019-11-09 22:41:33 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,5 +5,19 @@ class Pokemon
  def initialize(id:, name:, type:, db:)
  end
 
+ ef save
+   if self.id
+     self.update
+   else
+     sql = <<-SQL
+       INSERT INTO pokemon (name, type)
+       VALUES (?,?)
+     SQL
+
+     DB[:conn].execute(sql, self.name, self.type)
+     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+   end
+ end
+
 
 end
